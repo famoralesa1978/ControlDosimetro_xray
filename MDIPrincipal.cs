53eee82c8070b02ba97ef7c9e386e63d5567891c
fmoralesa1978 <famoralesa1978@gmail.com>
2020-05-20 12:43:20 -0400

    se borro un codigo sin querer

--- MDIPrincipal.cs
+++ MDIPrincipal.cs
@@ -101,8 +101,9 @@ namespace ControlDosimetro
          }
         private void MnuMantCliente_Click(object sender, EventArgs e)
         {
-		     
-            .
+            frmBusquedaEmpresa frm = new frmBusquedaEmpresa();
+            Graba_log(frm.Text);
+            frm.ShowDialog(this);     
         }
 
 		  private void MnuMantPersonal_Click(object sender, EventArgs e)
