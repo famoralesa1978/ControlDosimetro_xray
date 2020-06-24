0dd8bd84a51ff204577291602cb52f4a6649671f
beth.quinn@outlook.es <beth.quinn@outlook.es>
2020-06-24 07:13:22 -0500

    wait y default en boton grabar en frmSucursalMant

--- Mantenedor/frmSucursalMant.cs
+++ Mantenedor/frmSucursalMant.cs
@@ -216,6 +216,8 @@ namespace ControlDosimetro
 
         private void btn_Grabar_Click(object sender, EventArgs e)
         {
+            Cursor = Cursors.WaitCursor;
+
 			  Boolean bolResult;
 			  bolResult = false;
             if (MessageBox.Show("Desea grabar la información", "mensaje", MessageBoxButtons.OKCancel) == System.Windows.Forms.DialogResult.OK)
@@ -250,6 +252,8 @@ namespace ControlDosimetro
                 }
 					
             }
+
+            Cursor = Cursors.Default;
             
         }
 
