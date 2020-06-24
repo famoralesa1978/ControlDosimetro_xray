f5846e4316a37fed7e0bbd79108b025a35f8ee99
beth.quinn@outlook.es <beth.quinn@outlook.es>
2020-06-24 07:14:37 -0500

    wait y default en boton cerrar en frSucursalMant

--- Mantenedor/frmSucursalMant.cs
+++ Mantenedor/frmSucursalMant.cs
@@ -211,7 +211,11 @@ namespace ControlDosimetro
 
         private void btn_cerrar_Click(object sender, EventArgs e)
         {
+            Cursor = Cursors.WaitCursor;
+
             this.Close();
+
+            Cursor = Cursors.Default;
         }
 
         private void btn_Grabar_Click(object sender, EventArgs e)
