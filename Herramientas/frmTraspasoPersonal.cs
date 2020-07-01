39d35fc171a8bed3c06e387571ef3049736f60e5
fmoralesa1978 <famoralesa1978@gmail.com>
2020-07-01 02:00:39 -0400

    Diseño del formulario traspaso

---
+++ Herramientas/frmTraspasoPersonal.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using dllConectorMysql;
+using dllLibreriaEvento;
+using dllLibreriaMysql;
+
+namespace ControlDosimetro
+{
+    public partial class frmTraspasoPersonal : Form
+    {
+        #region "Definicion variable"
+        clsConectorSqlServer Conectar = new clsConectorSqlServer();
+        clsSqlComunSqlserver ClaseComun = new clsSqlComunSqlserver();
+        clsEventoControl ClaseEvento = new clsEventoControl();
+        //    Int64 intTempId_Estado;
+        //   Int64 intTempId_Cliente;
+        //   Int64 intTempPeriodo;
+        //    int intContar = 0;
+        #endregion
+
+        private void frmTraspasoPersonal_Load(object sender, EventArgs e)
+        {
+        }
+        public frmTraspasoPersonal(Int64 intId_Cliente)
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Cargar_cliente_Click(object sender, EventArgs e)
+        {
+            if(((Button)sender).Name==btn_Cargar_cliente.Name)
+                Cargar_Cliente(true);
+            if (((Button)sender).Name == btn_Cargar_clienteDestino.Name)
+                Cargar_Cliente(false);
+        }
+
+        private void Cargar_Cliente(Boolean bolOrigen)
+        {
+            string strRut = bolOrigen == true?txt_Rut.Text: txt_RutDestino.Text;
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "select id_cliente,run,Razon_Social,N_Cliente_Ref,region + ','+ comuna +','+Direccion as Direccion ,r.Id_Region,c.Id_Provincia,c.Id_Comuna,Telefono, Id_TipoFuente,Id_estado,Fechainicio " +
+                            "  FROM tbl_cliente c inner join [dbo].[glo_region] r on c.Id_Region=r.Id_Region inner join glo_comuna co on co.id_comuna=c.id_comuna" +
+                            " WHERE run= '" + strRut + "'";
+            DataSet dt;
+
+            dt = Conectar.Listar(Clases.clsBD.BD,cmd);
+            if (dt.Tables[0].Rows.Count > 0)
+            {
+                if (bolOrigen == true)
+                {
+                    txt_Rut.Text = strRut;
+                    lbl_RazonSocial.Text = dt.Tables[0].Rows[0]["Razon_Social"].ToString();
+                    lbl_Id_cliente.Text = dt.Tables[0].Rows[0]["id_cliente"].ToString();
+                }
+                else
+                {
+                    txt_RutDestino.Text = strRut;
+                    lbl_RazonSocialDestino.Text = dt.Tables[0].Rows[0]["Razon_Social"].ToString();
+                    lbl_Id_clienteDestino.Text = dt.Tables[0].Rows[0]["id_cliente"].ToString();
+                }         
+            }
+            else
+            {
+                if (bolOrigen == true)
+                {
+                    btn_Cargar_cliente.Enabled = true;
+
+                    txt_Rut.Enabled = true;
+                    lbl_Id_cliente.Text = "";
+                    lbl_RazonSocial.Text = "";
+                }
+                else {
+                    btn_Cargar_clienteDestino.Enabled = true;
+
+                    txt_RutDestino.Enabled = true;
+                    lbl_Id_clienteDestino.Text = "";
+                    lbl_RazonSocialDestino.Text = "";
+                }
+            }
+            if (lbl_Id_cliente.Text != "")
+                MessageBox.Show("El cliente no existe");
+        }
+    }
+}
---
+++ Herramientas/frmTraspasoPersonal.designer.cs
@@ -0,0 +1,285 @@
+﻿namespace ControlDosimetro
+{
+    partial class frmTraspasoPersonal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbl_Id_cliente = new System.Windows.Forms.Label();
+            this.lbl_cliente = new System.Windows.Forms.Label();
+            this.lbl_RazonSocial = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btn_Cargar_cliente = new System.Windows.Forms.Button();
+            this.txt_Rut = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lbl_Id_clienteDestino = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_RazonSocialDestino = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_Cargar_clienteDestino = new System.Windows.Forms.Button();
+            this.txt_RutDestino = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox2);
+            this.splitContainer1.Size = new System.Drawing.Size(760, 409);
+            this.splitContainer1.SplitterDistance = 356;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.lbl_Id_cliente);
+            this.groupBox1.Controls.Add(this.lbl_cliente);
+            this.groupBox1.Controls.Add(this.lbl_RazonSocial);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.btn_Cargar_cliente);
+            this.groupBox1.Controls.Add(this.txt_Rut);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(356, 409);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dato personal";
+            // 
+            // lbl_Id_cliente
+            // 
+            this.lbl_Id_cliente.AutoSize = true;
+            this.lbl_Id_cliente.Location = new System.Drawing.Point(88, 72);
+            this.lbl_Id_cliente.Name = "lbl_Id_cliente";
+            this.lbl_Id_cliente.Size = new System.Drawing.Size(69, 13);
+            this.lbl_Id_cliente.TabIndex = 51;
+            this.lbl_Id_cliente.Text = "lbl_Id_cliente";
+            // 
+            // lbl_cliente
+            // 
+            this.lbl_cliente.AutoSize = true;
+            this.lbl_cliente.Location = new System.Drawing.Point(10, 72);
+            this.lbl_cliente.Name = "lbl_cliente";
+            this.lbl_cliente.Size = new System.Drawing.Size(54, 13);
+            this.lbl_cliente.TabIndex = 50;
+            this.lbl_cliente.Text = "N° Cliente";
+            // 
+            // lbl_RazonSocial
+            // 
+            this.lbl_RazonSocial.AutoSize = true;
+            this.lbl_RazonSocial.Location = new System.Drawing.Point(88, 49);
+            this.lbl_RazonSocial.Name = "lbl_RazonSocial";
+            this.lbl_RazonSocial.Size = new System.Drawing.Size(83, 13);
+            this.lbl_RazonSocial.TabIndex = 49;
+            this.lbl_RazonSocial.Text = "lbl_RazonSocial";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(10, 24);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(24, 13);
+            this.label11.TabIndex = 48;
+            this.label11.Text = "Rut";
+            // 
+            // btn_Cargar_cliente
+            // 
+            this.btn_Cargar_cliente.Location = new System.Drawing.Point(206, 19);
+            this.btn_Cargar_cliente.Name = "btn_Cargar_cliente";
+            this.btn_Cargar_cliente.Size = new System.Drawing.Size(119, 23);
+            this.btn_Cargar_cliente.TabIndex = 47;
+            this.btn_Cargar_cliente.Text = "Cargar Cliente";
+            this.btn_Cargar_cliente.UseVisualStyleBackColor = true;
+            this.btn_Cargar_cliente.Click += new System.EventHandler(this.btn_Cargar_cliente_Click);
+            // 
+            // txt_Rut
+            // 
+            this.txt_Rut.Location = new System.Drawing.Point(91, 21);
+            this.txt_Rut.Name = "txt_Rut";
+            this.txt_Rut.Size = new System.Drawing.Size(112, 20);
+            this.txt_Rut.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 34;
+            this.label2.Text = "Razon social";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lbl_Id_clienteDestino);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.lbl_RazonSocialDestino);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.btn_Cargar_clienteDestino);
+            this.groupBox2.Controls.Add(this.txt_RutDestino);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(400, 409);
+            this.groupBox2.TabIndex = 47;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Listado personal";
+            // 
+            // lbl_Id_clienteDestino
+            // 
+            this.lbl_Id_clienteDestino.AutoSize = true;
+            this.lbl_Id_clienteDestino.Location = new System.Drawing.Point(89, 70);
+            this.lbl_Id_clienteDestino.Name = "lbl_Id_clienteDestino";
+            this.lbl_Id_clienteDestino.Size = new System.Drawing.Size(35, 13);
+            this.lbl_Id_clienteDestino.TabIndex = 58;
+            this.lbl_Id_clienteDestino.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 57;
+            this.label3.Text = "N° Cliente";
+            // 
+            // lbl_RazonSocialDestino
+            // 
+            this.lbl_RazonSocialDestino.AutoSize = true;
+            this.lbl_RazonSocialDestino.Location = new System.Drawing.Point(89, 47);
+            this.lbl_RazonSocialDestino.Name = "lbl_RazonSocialDestino";
+            this.lbl_RazonSocialDestino.Size = new System.Drawing.Size(35, 13);
+            this.lbl_RazonSocialDestino.TabIndex = 56;
+            this.lbl_RazonSocialDestino.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 13);
+            this.label5.TabIndex = 55;
+            this.label5.Text = "Rut";
+            // 
+            // btn_Cargar_clienteDestino
+            // 
+            this.btn_Cargar_clienteDestino.Location = new System.Drawing.Point(207, 17);
+            this.btn_Cargar_clienteDestino.Name = "btn_Cargar_clienteDestino";
+            this.btn_Cargar_clienteDestino.Size = new System.Drawing.Size(119, 23);
+            this.btn_Cargar_clienteDestino.TabIndex = 54;
+            this.btn_Cargar_clienteDestino.Text = "Cargar Cliente";
+            this.btn_Cargar_clienteDestino.UseVisualStyleBackColor = true;
+            this.btn_Cargar_clienteDestino.Click += new System.EventHandler(this.btn_Cargar_cliente_Click);
+            // 
+            // txt_RutDestino
+            // 
+            this.txt_RutDestino.Location = new System.Drawing.Point(92, 19);
+            this.txt_RutDestino.Name = "txt_RutDestino";
+            this.txt_RutDestino.Size = new System.Drawing.Size(112, 20);
+            this.txt_RutDestino.TabIndex = 52;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 47);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 13);
+            this.label6.TabIndex = 53;
+            this.label6.Text = "Razon social";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 100);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(326, 262);
+            this.dataGridView1.TabIndex = 52;
+            // 
+            // frmTraspasoPersonal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 409);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "frmTraspasoPersonal";
+            this.Text = "Traspaso";
+            this.Load += new System.EventHandler(this.frmTraspasoPersonal_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button btn_Cargar_cliente;
+        private System.Windows.Forms.TextBox txt_Rut;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lbl_cliente;
+        private System.Windows.Forms.Label lbl_RazonSocial;
+        private System.Windows.Forms.Label lbl_Id_cliente;
+        private System.Windows.Forms.Label lbl_Id_clienteDestino;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbl_RazonSocialDestino;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn_Cargar_clienteDestino;
+        private System.Windows.Forms.TextBox txt_RutDestino;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
