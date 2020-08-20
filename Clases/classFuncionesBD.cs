7d10885cc20b7ccced1a9f92c75bfefde15a8f70
felipe morales <fmorales_arenas@hotmail.com>
2020-08-20 00:24:33 -0400

    agregar

--- Clases/classFuncionesBD.cs
+++ Clases/classFuncionesBD.cs
@@ -27,6 +27,20 @@ namespace classFuncionesBD
             mensaje=ds.Tables[0].Rows[0][1].ToString();
             return Convert.ToInt16( ds.Tables[0].Rows[0][0].ToString());
         }
-    }
+
+				public int CambiarEstadoTLD(string NDoc, string NDos, ref string mensaje)
+				{
+					SqlCommand cmd = new SqlCommand();
+					DataSet ds;
+
+					cmd.CommandText = "pa_CambiarEstadoTLD_upd " + NDoc + "," + NDos;
+					cmd.CommandType = CommandType.Text;
+					ds = Conectar.Listar(Clases.clsBD.BD, cmd);
+
+					mensaje = ds.Tables[0].Rows[0][1].ToString();
+					return Convert.ToInt16(ds.Tables[0].Rows[0][0].ToString());
+				}
+
+	}
 
 }
