8ffbcf5ef0a9330ca331a5b241e0f2bf6166d6ab
beth.quinn@outlook.es <beth.quinn@outlook.es>
2020-08-27 02:40:45 -0500

    Formulario de cambiar fecha quedo centrado en pantalla

--- Herramientas/frmCambioFecha.Designer.cs
+++ Herramientas/frmCambioFecha.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_Guardar = new System.Windows.Forms.Button();
+            this.dtp_FechaDestino = new System.Windows.Forms.DateTimePicker();
+            this.lbl_FechaDestino = new System.Windows.Forms.Label();
             this.btn_Modificar = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.dtp_FechaInicio = new System.Windows.Forms.DateTimePicker();
@@ -44,6 +47,9 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.btn_Guardar);
+            this.groupBox1.Controls.Add(this.dtp_FechaDestino);
+            this.groupBox1.Controls.Add(this.lbl_FechaDestino);
             this.groupBox1.Controls.Add(this.btn_Modificar);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.dtp_FechaInicio);
@@ -61,19 +67,48 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Datos";
             // 
+            // btn_Guardar
+            // 
+            this.btn_Guardar.Location = new System.Drawing.Point(215, 157);
+            this.btn_Guardar.Name = "btn_Guardar";
+            this.btn_Guardar.Size = new System.Drawing.Size(88, 23);
+            this.btn_Guardar.TabIndex = 12;
+            this.btn_Guardar.Text = "Guardar";
+            this.btn_Guardar.UseVisualStyleBackColor = true;
+            this.btn_Guardar.Click += new System.EventHandler(this.btn_Guardar_Click);
+            // 
+            // dtp_FechaDestino
+            // 
+            this.dtp_FechaDestino.CustomFormat = "dd/MM/yyyy";
+            this.dtp_FechaDestino.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtp_FechaDestino.Location = new System.Drawing.Point(108, 157);
+            this.dtp_FechaDestino.Name = "dtp_FechaDestino";
+            this.dtp_FechaDestino.Size = new System.Drawing.Size(101, 20);
+            this.dtp_FechaDestino.TabIndex = 11;
+            // 
+            // lbl_FechaDestino
+            // 
+            this.lbl_FechaDestino.AutoSize = true;
+            this.lbl_FechaDestino.Location = new System.Drawing.Point(23, 163);
+            this.lbl_FechaDestino.Name = "lbl_FechaDestino";
+            this.lbl_FechaDestino.Size = new System.Drawing.Size(79, 13);
+            this.lbl_FechaDestino.TabIndex = 10;
+            this.lbl_FechaDestino.Text = "Fecha Destino:";
+            // 
             // btn_Modificar
             // 
-            this.btn_Modificar.Location = new System.Drawing.Point(194, 153);
+            this.btn_Modificar.Location = new System.Drawing.Point(215, 126);
             this.btn_Modificar.Name = "btn_Modificar";
-            this.btn_Modificar.Size = new System.Drawing.Size(109, 23);
+            this.btn_Modificar.Size = new System.Drawing.Size(88, 23);
             this.btn_Modificar.TabIndex = 9;
             this.btn_Modificar.Text = "Modificar";
             this.btn_Modificar.UseVisualStyleBackColor = true;
+            this.btn_Modificar.Click += new System.EventHandler(this.btn_Modificar_Click_1);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(23, 158);
+            this.label4.Location = new System.Drawing.Point(23, 134);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(68, 13);
             this.label4.TabIndex = 8;
@@ -81,26 +116,27 @@
             // 
             // dtp_FechaInicio
             // 
-            this.dtp_FechaInicio.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtp_FechaInicio.Location = new System.Drawing.Point(93, 153);
+            this.dtp_FechaInicio.CustomFormat = "dd/MM/yyyy";
+            this.dtp_FechaInicio.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtp_FechaInicio.Location = new System.Drawing.Point(108, 126);
             this.dtp_FechaInicio.Name = "dtp_FechaInicio";
-            this.dtp_FechaInicio.Size = new System.Drawing.Size(91, 20);
+            this.dtp_FechaInicio.Size = new System.Drawing.Size(101, 20);
             this.dtp_FechaInicio.TabIndex = 7;
             // 
             // lbl_nombrecliente
             // 
             this.lbl_nombrecliente.AutoSize = true;
-            this.lbl_nombrecliente.Location = new System.Drawing.Point(93, 85);
+            this.lbl_nombrecliente.Location = new System.Drawing.Point(108, 78);
             this.lbl_nombrecliente.Name = "lbl_nombrecliente";
-            this.lbl_nombrecliente.Size = new System.Drawing.Size(35, 13);
+            this.lbl_nombrecliente.Size = new System.Drawing.Size(70, 13);
             this.lbl_nombrecliente.TabIndex = 6;
-            this.lbl_nombrecliente.Text = "label5";
+            this.lbl_nombrecliente.Text = "Razón Social";
             // 
             // btn_CargarCliente
             // 
-            this.btn_CargarCliente.Location = new System.Drawing.Point(194, 42);
+            this.btn_CargarCliente.Location = new System.Drawing.Point(215, 47);
             this.btn_CargarCliente.Name = "btn_CargarCliente";
-            this.btn_CargarCliente.Size = new System.Drawing.Size(109, 23);
+            this.btn_CargarCliente.Size = new System.Drawing.Size(88, 23);
             this.btn_CargarCliente.TabIndex = 5;
             this.btn_CargarCliente.Text = "Cargar Cliente";
             this.btn_CargarCliente.UseVisualStyleBackColor = true;
@@ -108,7 +144,7 @@
             // 
             // lbl_id_cliente
             // 
-            this.lbl_id_cliente.Location = new System.Drawing.Point(93, 44);
+            this.lbl_id_cliente.Location = new System.Drawing.Point(108, 47);
             this.lbl_id_cliente.MaxLength = 10;
             this.lbl_id_cliente.Name = "lbl_id_cliente";
             this.lbl_id_cliente.Size = new System.Drawing.Size(91, 20);
@@ -117,16 +153,16 @@
             // lbl_run
             // 
             this.lbl_run.AutoSize = true;
-            this.lbl_run.Location = new System.Drawing.Point(93, 119);
+            this.lbl_run.Location = new System.Drawing.Point(108, 102);
             this.lbl_run.Name = "lbl_run";
-            this.lbl_run.Size = new System.Drawing.Size(35, 13);
+            this.lbl_run.Size = new System.Drawing.Size(64, 13);
             this.lbl_run.TabIndex = 3;
-            this.lbl_run.Text = "label4";
+            this.lbl_run.Text = "12345678-9";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(23, 121);
+            this.label3.Location = new System.Drawing.Point(23, 105);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(34, 13);
             this.label3.TabIndex = 2;
@@ -135,7 +171,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(23, 84);
+            this.label2.Location = new System.Drawing.Point(23, 76);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(47, 13);
             this.label2.TabIndex = 1;
@@ -152,9 +188,13 @@
             // 
             // frmCambioFecha
             // 
-            this.ClientSize = new System.Drawing.Size(410, 233);
+            this.ClientSize = new System.Drawing.Size(409, 233);
             this.Controls.Add(this.groupBox1);
+            this.MaximumSize = new System.Drawing.Size(425, 272);
+            this.MinimumSize = new System.Drawing.Size(425, 272);
             this.Name = "frmCambioFecha";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Modificar Fecha";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
@@ -174,5 +214,8 @@
         private System.Windows.Forms.Button btn_Modificar;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.DateTimePicker dtp_FechaInicio;
+        private System.Windows.Forms.Button btn_Guardar;
+        private System.Windows.Forms.DateTimePicker dtp_FechaDestino;
+        private System.Windows.Forms.Label lbl_FechaDestino;
     }
 }
--- Herramientas/frmCambioFecha.cs
+++ Herramientas/frmCambioFecha.cs
@@ -29,6 +29,16 @@ namespace ControlDosimetro.Herramientas
         public frmCambioFecha(Int64 intId_Cliente)
         {
             InitializeComponent();
+
+            AsignarEvento();
+            dtp_FechaDestino.Enabled = false;
+            lbl_FechaDestino.Enabled = false;
+            btn_Guardar.Enabled = false;
+            btn_Modificar.Enabled = false;
+            dtp_FechaDestino.Visible = false;
+            lbl_FechaDestino.Visible = false;
+            btn_Guardar.Visible = false;
+            btn_Modificar.Visible = false;
         }
 
         #endregion
@@ -48,7 +58,10 @@ namespace ControlDosimetro.Herramientas
                 lbl_id_cliente.Text = intCodCliente.ToString();
                 lbl_run.Text = dt.Tables[0].Rows[0]["run"].ToString();
                 lbl_nombrecliente.Text = dt.Tables[0].Rows[0]["Razon_Social"].ToString();
-                dtp_FechaInicio.Text= dt.Tables[0].Rows[0]["FechaInicio"].ToString();
+                dtp_FechaInicio.Text = dt.Tables[0].Rows[0]["FechaInicio"].ToString();
+                ;
+                btn_Modificar.Visible = true;
+                btn_Modificar.Enabled = true;
             }
             else
             {
@@ -63,7 +76,28 @@ namespace ControlDosimetro.Herramientas
 
         }
 
+        private void AsignarEvento()
+        {
+            lbl_id_cliente.KeyPress += new KeyPressEventHandler(ClaseEvento.Numero_KeyPress);
+            lbl_id_cliente.KeyDown += new KeyEventHandler(ClaseEvento.Numero_KeyDown);
+        }
 
+        private void LimpiarControl()
+        {
+
+            lbl_id_cliente.Text = "";
+            lbl_nombrecliente.Text = "Razón Social";
+            lbl_run.Text = "12345678-9";
+            dtp_FechaInicio.Enabled = true;
+            dtp_FechaDestino.Enabled = false;
+            lbl_FechaDestino.Enabled = false;
+            btn_Guardar.Enabled = false;
+            btn_Modificar.Enabled = false;
+            dtp_FechaDestino.Visible = false;
+            lbl_FechaDestino.Visible = false;
+            btn_Guardar.Visible = false;
+            btn_Modificar.Visible = false;
+        }
 
         #endregion
 
@@ -72,13 +106,66 @@ namespace ControlDosimetro.Herramientas
         private void btn_CargarCliente_Click(object sender, EventArgs e)
         {
             Cursor = Cursors.WaitCursor;
-            Cargar_Cliente(Convert.ToInt64(lbl_id_cliente.Text));
+          
+            if (!String.IsNullOrEmpty(lbl_id_cliente.Text))
+            {
+                Cargar_Cliente(Convert.ToInt64(lbl_id_cliente.Text));
+            }
+            else
+            {
+                MessageBox.Show("Debe ingresar el N° de Cliente");
+            }
             Cursor = Cursors.Default;
+
+        }
+
+        private void btn_Modificar_Click_1(object sender, EventArgs e)
+        {
+            Cursor = Cursors.WaitCursor;
+            dtp_FechaInicio.Enabled = false;
+            dtp_FechaDestino.Enabled = true;
+            lbl_FechaDestino.Enabled = true;
+            btn_Guardar.Enabled = true;
+            dtp_FechaDestino.Visible = true;
+            lbl_FechaDestino.Visible = true;
+            btn_Guardar.Visible = true;
+            Cursor = Cursors.Default;
+        }
+
+        private void btn_Guardar_Click(object sender, EventArgs e)
+        {
+            string fecha = dtp_FechaDestino.Value.ToShortDateString();
+
+            SqlCommand cmd = new SqlCommand();
+            DataSet ds;
+            string strParametro = String.Format("{0},'{1}','{2}','{3}','{4}'", lbl_id_cliente.Text, lbl_run.Text, lbl_nombrecliente.Text, dtp_FechaInicio.Text, dtp_FechaDestino.Text);
+            cmd.CommandText = "pa_CambiarFecha_upd " + strParametro;
+            cmd.CommandType = CommandType.Text;
+            
+
+            ds = Conectar.Listar(Clases.clsBD.BD, cmd);
+            if (Convert.ToInt16( ds.Tables[0].Rows[0][0].ToString()) != 0)
+            {
+                MessageBox.Show("Error en actualizar la información");
+            }
+
+            else
+                MessageBox.Show(ds.Tables[0].Rows[0][1].ToString());
+
+            LimpiarControl();
+
         }
 
 
         #endregion
-        
-     
+
+
+
+        private void dtp_FechaDestino_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime fecha = dtp_FechaDestino.Value;
+        }
+
+      
     }
 }
