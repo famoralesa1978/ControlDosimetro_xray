5c0e034aa1e74c4fac0457a0f3ca1fe37bd75a66
felipe morales <fmorales_arenas@hotmail.com>
2020-11-25 23:52:09 -0400

    Listar detalle y aplicar delete en  mantenedor  de permiso

--- Mantenedor/Sistema/frmMantenedorPermiso.Designer.cs
+++ Mantenedor/Sistema/frmMantenedorPermiso.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
 			this.stsEstado = new System.Windows.Forms.StatusStrip();
 			this.tssDescEstado = new System.Windows.Forms.ToolStripStatusLabel();
 			this.tssEstado = new System.Windows.Forms.ToolStripStatusLabel();
@@ -43,6 +43,12 @@
 			this.Colperfil1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
 			this.tbl_perfil = new System.Windows.Forms.GroupBox();
 			this.dgvDetalle = new System.Windows.Forms.DataGridView();
+			this.ColMenu = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.ColAcceso = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+			this.ColLectura = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+			this.ColNuevo = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+			this.ColModificacion = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+			this.ColEliminar = new System.Windows.Forms.DataGridViewCheckBoxColumn();
 			this.stsEstado.SuspendLayout();
 			this.toolStrip1.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.scPrincipal)).BeginInit();
@@ -63,7 +69,7 @@
             this.tssEstado});
 			this.stsEstado.Location = new System.Drawing.Point(0, 489);
 			this.stsEstado.Name = "stsEstado";
-			this.stsEstado.Size = new System.Drawing.Size(629, 22);
+			this.stsEstado.Size = new System.Drawing.Size(969, 22);
 			this.stsEstado.TabIndex = 0;
 			this.stsEstado.Text = "statusStrip1";
 			// 
@@ -88,7 +94,7 @@
             this.tsbAgregar});
 			this.toolStrip1.Location = new System.Drawing.Point(0, 0);
 			this.toolStrip1.Name = "toolStrip1";
-			this.toolStrip1.Size = new System.Drawing.Size(629, 25);
+			this.toolStrip1.Size = new System.Drawing.Size(969, 25);
 			this.toolStrip1.TabIndex = 1;
 			this.toolStrip1.Text = "toolStrip1";
 			// 
@@ -138,8 +144,8 @@
 			this.scPrincipal.Panel2.Controls.Add(this.tbl_perfil);
 			this.scPrincipal.Panel2.RightToLeft = System.Windows.Forms.RightToLeft.No;
 			this.scPrincipal.Panel2MinSize = 200;
-			this.scPrincipal.Size = new System.Drawing.Size(617, 461);
-			this.scPrincipal.SplitterDistance = 250;
+			this.scPrincipal.Size = new System.Drawing.Size(957, 461);
+			this.scPrincipal.SplitterDistance = 309;
 			this.scPrincipal.TabIndex = 2;
 			// 
 			// pnlPrincipal
@@ -152,7 +158,7 @@
 			this.pnlPrincipal.Location = new System.Drawing.Point(0, 3);
 			this.pnlPrincipal.Margin = new System.Windows.Forms.Padding(6, 3, 5, 3);
 			this.pnlPrincipal.Name = "pnlPrincipal";
-			this.pnlPrincipal.Size = new System.Drawing.Size(245, 455);
+			this.pnlPrincipal.Size = new System.Drawing.Size(304, 455);
 			this.pnlPrincipal.TabIndex = 1;
 			// 
 			// gpxListado
@@ -161,7 +167,7 @@
 			this.gpxListado.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.gpxListado.Location = new System.Drawing.Point(0, 0);
 			this.gpxListado.Name = "gpxListado";
-			this.gpxListado.Size = new System.Drawing.Size(243, 453);
+			this.gpxListado.Size = new System.Drawing.Size(302, 453);
 			this.gpxListado.TabIndex = 4;
 			this.gpxListado.TabStop = false;
 			this.gpxListado.Text = "Listado";
@@ -172,14 +178,14 @@
 			this.dgvGrilla.AllowUserToDeleteRows = false;
 			this.dgvGrilla.AllowUserToOrderColumns = true;
 			this.dgvGrilla.AllowUserToResizeColumns = false;
-			dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopLeft;
-			dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
-			dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
-			dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-			dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-			dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-			this.dgvGrilla.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+			dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopLeft;
+			dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+			dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+			dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+			dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+			dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+			this.dgvGrilla.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
 			this.dgvGrilla.ColumnHeadersHeight = 42;
 			this.dgvGrilla.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Colperfil1});
@@ -187,9 +193,9 @@
 			this.dgvGrilla.Location = new System.Drawing.Point(3, 16);
 			this.dgvGrilla.Name = "dgvGrilla";
 			this.dgvGrilla.ReadOnly = true;
-			this.dgvGrilla.Size = new System.Drawing.Size(237, 434);
+			this.dgvGrilla.Size = new System.Drawing.Size(296, 434);
 			this.dgvGrilla.TabIndex = 0;
-			this.dgvGrilla.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvGrilla_CellDoubleClick);
+			this.dgvGrilla.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvGrilla_CellClick);
 			this.dgvGrilla.ColumnWidthChanged += new System.Windows.Forms.DataGridViewColumnEventHandler(this.dgvGrilla_ColumnWidthChanged);
 			this.dgvGrilla.Paint += new System.Windows.Forms.PaintEventHandler(this.dgvGrilla_Paint);
 			// 
@@ -207,25 +213,77 @@
 			this.tbl_perfil.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.tbl_perfil.Location = new System.Drawing.Point(0, 0);
 			this.tbl_perfil.Name = "tbl_perfil";
-			this.tbl_perfil.Size = new System.Drawing.Size(363, 461);
+			this.tbl_perfil.Size = new System.Drawing.Size(644, 461);
 			this.tbl_perfil.TabIndex = 6;
 			this.tbl_perfil.TabStop = false;
 			this.tbl_perfil.Text = "Permiso";
 			// 
 			// dgvDetalle
 			// 
+			this.dgvDetalle.AllowUserToAddRows = false;
+			this.dgvDetalle.AllowUserToDeleteRows = false;
+			this.dgvDetalle.AllowUserToResizeColumns = false;
+			this.dgvDetalle.AllowUserToResizeRows = false;
 			this.dgvDetalle.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvDetalle.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColMenu,
+            this.ColAcceso,
+            this.ColLectura,
+            this.ColNuevo,
+            this.ColModificacion,
+            this.ColEliminar});
 			this.dgvDetalle.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.dgvDetalle.Location = new System.Drawing.Point(3, 16);
 			this.dgvDetalle.Name = "dgvDetalle";
-			this.dgvDetalle.Size = new System.Drawing.Size(357, 442);
+			this.dgvDetalle.Size = new System.Drawing.Size(638, 442);
 			this.dgvDetalle.TabIndex = 0;
+			// 
+			// ColMenu
+			// 
+			this.ColMenu.DataPropertyName = "Menu";
+			this.ColMenu.HeaderText = "Menu";
+			this.ColMenu.Name = "ColMenu";
+			this.ColMenu.ReadOnly = true;
+			this.ColMenu.Width = 130;
+			// 
+			// ColAcceso
+			// 
+			this.ColAcceso.DataPropertyName = "Acceso";
+			this.ColAcceso.HeaderText = "Acceso";
+			this.ColAcceso.Name = "ColAcceso";
+			this.ColAcceso.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+			this.ColAcceso.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+			this.ColAcceso.Width = 80;
+			// 
+			// ColLectura
+			// 
+			this.ColLectura.DataPropertyName = "Lectura";
+			this.ColLectura.HeaderText = "Lectura";
+			this.ColLectura.Name = "ColLectura";
+			// 
+			// ColNuevo
+			// 
+			this.ColNuevo.DataPropertyName = "Nuevo";
+			this.ColNuevo.HeaderText = "Nuevo";
+			this.ColNuevo.Name = "ColNuevo";
+			// 
+			// ColModificacion
+			// 
+			this.ColModificacion.DataPropertyName = "Modificacion";
+			this.ColModificacion.HeaderText = "Modificación";
+			this.ColModificacion.Name = "ColModificacion";
+			// 
+			// ColEliminar
+			// 
+			this.ColEliminar.DataPropertyName = "Eliminar";
+			this.ColEliminar.HeaderText = "Eliminar";
+			this.ColEliminar.Name = "ColEliminar";
 			// 
 			// frmMantenedorPermiso
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(629, 511);
+			this.ClientSize = new System.Drawing.Size(969, 511);
 			this.Controls.Add(this.scPrincipal);
 			this.Controls.Add(this.toolStrip1);
 			this.Controls.Add(this.stsEstado);
@@ -270,5 +328,11 @@
         private System.Windows.Forms.DataGridView dgvGrilla;
         private System.Windows.Forms.DataGridViewTextBoxColumn Colperfil1;
 		private System.Windows.Forms.DataGridView dgvDetalle;
+		private System.Windows.Forms.DataGridViewTextBoxColumn ColMenu;
+		private System.Windows.Forms.DataGridViewCheckBoxColumn ColAcceso;
+		private System.Windows.Forms.DataGridViewCheckBoxColumn ColLectura;
+		private System.Windows.Forms.DataGridViewCheckBoxColumn ColNuevo;
+		private System.Windows.Forms.DataGridViewCheckBoxColumn ColModificacion;
+		private System.Windows.Forms.DataGridViewCheckBoxColumn ColEliminar;
 	}
 }
--- Mantenedor/Sistema/frmMantenedorPermiso.cs
+++ Mantenedor/Sistema/frmMantenedorPermiso.cs
@@ -15,91 +15,117 @@ using System.Data.Sql;
 
 namespace ControlDosimetro
 {
-    public partial class frmMantenedorPermiso : Form
-    {
-        #region "Definicion variable"
+	public partial class frmMantenedorPermiso : Form
+	{
+		#region "Definicion variable"
 
-        TextBox txtBox = new TextBox();
-        enum ConfGrilla: int
-        {
-            id=0,
-            descripcion=1,
-            Id_estado = 2
-        };
+		TextBox txtBox = new TextBox();
+		enum ConfGrilla : int
+		{
+			id = 0,
+			descripcion = 1,
+			Id_estado = 2
+		};
 
-        clsConectorSqlServer Conectar = new clsConectorSqlServer();
-        clsSqlComunSqlserver ClaseComun = new clsSqlComunSqlserver();
-        clsEventoControl ClaseEvento = new clsEventoControl();
+		enum ConfGrillaDetalle : int
+		{
+			idmenu = 0,
+			Menu=1,
+			Acceso=2,
+			Lectura=3,
+			Nuevo=4,
+			Modificacion=5,
+			Eliminar=6
+	};
 
-        #endregion
+		clsConectorSqlServer Conectar = new clsConectorSqlServer();
+		clsSqlComunSqlserver ClaseComun = new clsSqlComunSqlserver();
+		clsEventoControl ClaseEvento = new clsEventoControl();
 
-        #region "Inicio"
+		#endregion
 
-        public frmMantenedorPermiso()
-        {
-            InitializeComponent();
-          //  scPrincipal.Panel2Collapsed = true;
-            tsbGuardar.Enabled = false;
-            dgvGrilla.AutoGenerateColumns = false;
-        }
+		#region "Inicio"
 
-        private void frmMantenedorPermiso_Load(object sender, EventArgs e)
-        {            
-            CargarGrilla();
-        }
+		public frmMantenedorPermiso()
+		{
+			InitializeComponent();
+			tsbGuardar.Enabled = false;
+			dgvGrilla.AutoGenerateColumns = false;
+			dgvDetalle.AutoGenerateColumns = false;
+		}
 
-        #endregion
+		private void frmMantenedorPermiso_Load(object sender, EventArgs e)
+		{
+			CargarGrilla();
+			LlamadoAModificar(dgvGrilla.CurrentCell.RowIndex);
+		}
 
-        #region Procedimiento
-    
-        private void Grabar()
-        {
-            //Boolean bolResult;
-            //bolResult = false;
-            //if (MessageBox.Show("Desea grabar la información", "mensaje", MessageBoxButtons.OKCancel) == System.Windows.Forms.DialogResult.OK)
-            //{
-            //    if ((tssEstado.Text == "Nuevo")&&(txt_Id_perfil.Text=="0"))
-            //    {
-            //        ClaseComun.Insertar(Clases.clsBD.BD,tbl_perfil, ref bolResult);
-            //        if (bolResult == true)
-            //        {
-            //            CargarGrilla();
-            //            MessageBox.Show("Dato Guardado");                        
-            //        }
-            //    }
-            //    else
-            //    if (tssEstado.Text == "Modificar")
-            //    {
-            //        ClaseComun.Modificar(Clases.clsBD.BD,tbl_perfil, ref bolResult);
-            //        if (bolResult == true)
-            //        {
-            //            CargarGrilla();
-            //            MessageBox.Show("Dato modificado");
-            //        }
-            //    }
-            //}
-        }
+		#endregion
 
-        private void CargarGrilla()
-        {
-            SqlCommand cmd = new SqlCommand();
-            cmd.CommandText = "SELECT  [Id_perfil] as id,Descripcion,Id_estado FROM [dbo].[tbl_perfil]  order by Descripcion";
+		#region Procedimiento
 
-            cmd.CommandType = CommandType.Text;
+		private void Grabar()
+		{
+			Boolean bolResult;
+			bolResult = false;
+			if (MessageBox.Show("Desea grabar la información", "mensaje", MessageBoxButtons.OKCancel) == System.Windows.Forms.DialogResult.OK)
+			{
+				Cursor = Cursors.WaitCursor;
+				DataTable dt = (DataTable)dgvGrilla.DataSource;
 
-            DataSet dt;
-            dt = Conectar.Listar(Clases.clsBD.BD,cmd);
+				DataRow currentRow = dt.Rows[dgvGrilla.CurrentCell.RowIndex];
+				SqlCommand cmd = new SqlCommand();
+				cmd.CommandText = "pa_Permiso_Del " + currentRow[ConfGrilla.id.ToString()].ToString();
+				cmd.CommandType = CommandType.Text;
+				Conectar.AgregarModificarEliminar(Clases.clsBD.BD, cmd);
 
-            dgvGrilla.DataSource = dt.Tables[0];
-        }
+				//pa_Permiso_Del
+				//        if (bolResult == true)
+				//        {
+				//            CargarGrilla();
+				//            MessageBox.Show("Dato Guardado");                        
+				//        }
+				//    }
+				//    else
+				//    if (tssEstado.Text == "Modificar")
+				//    {
+				//        ClaseComun.Modificar(Clases.clsBD.BD,tbl_perfil, ref bolResult);
+				//        if (bolResult == true)
+				//        {
+				//            CargarGrilla();
+				//            MessageBox.Show("Dato modificado");
+				//        }
+				//    }
+				Cursor = Cursors.Default;
+			}
+		}
+
+		private void CargarGrilla()
+		{
+			SqlCommand cmd = new SqlCommand();
+			cmd.CommandText = "SELECT  [Id_perfil] as id,Descripcion,Id_estado FROM [dbo].[tbl_perfil]  order by Descripcion";
+
+			cmd.CommandType = CommandType.Text;
+
+			DataSet dt;
+			dt = Conectar.Listar(Clases.clsBD.BD, cmd);
+
+			dgvGrilla.DataSource = dt.Tables[0];
+		}
 
 		private void LlamadoAModificar(int intFila)
 		{
 			DataTable dt = (DataTable)dgvGrilla.DataSource;
 			DataRow currentRow = dt.Rows[intFila];
-			//txt_Id_perfil.Text = currentRow[ConfGrilla.id.ToString()].ToString();
+			SqlCommand cmd = new SqlCommand();
+			cmd.CommandText = "pa_Permiso_sel " + currentRow[ConfGrilla.id.ToString()].ToString();
 
-			//pa_Permiso_sel currentRow[ConfGrilla.id.ToString()].ToString();
+			cmd.CommandType = CommandType.Text;
+
+			DataSet dtDet;
+			dtDet = Conectar.Listar(Clases.clsBD.BD, cmd);
+
+			dgvDetalle.DataSource = dtDet.Tables[0];
 
 			//txt_Descripcion.Text = currentRow[ConfGrilla.descripcion.ToString()].ToString();
 			//cbx_id_estado.SelectedValue = currentRow[ConfGrilla.Id_estado.ToString()].ToString();
@@ -122,52 +148,53 @@ namespace ControlDosimetro
 		#region " grilla"
 
 		private void dgvGrilla_Paint(object sender, PaintEventArgs e)
-        {
-            int columnIndex = 0;
-            Point headerCellLocation = this.dgvGrilla.GetCellDisplayRectangle(columnIndex, -1, true).Location;                     
-            txtBox.Location = new Point(headerCellLocation.X, headerCellLocation.Y+20);
-            txtBox.BackColor = Color.AliceBlue;
-            txtBox.Width = Colperfil1.Width;
-            txtBox.TextChanged += new EventHandler(TextBox_Changed);
-            dgvGrilla.Controls.Add(txtBox);
-        }
+		{
+			int columnIndex = 0;
+			Point headerCellLocation = this.dgvGrilla.GetCellDisplayRectangle(columnIndex, -1, true).Location;
+			txtBox.Location = new Point(headerCellLocation.X, headerCellLocation.Y + 20);
+			txtBox.BackColor = Color.AliceBlue;
+			txtBox.Width = Colperfil1.Width;
+			txtBox.TextChanged += new EventHandler(TextBox_Changed);
+			dgvGrilla.Controls.Add(txtBox);
+		}
 
-        private void dgvGrilla_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
-        {
-            int intFila = e.RowIndex;
-            LlamadoAModificar(intFila);
-        }
+		private void dgvGrilla_ColumnWidthChanged(object sender, DataGridViewColumnEventArgs e)
+		{
+			txtBox.Width = Colperfil1.Width;
+		}
 
-        private void TextBox_Changed(object sender, EventArgs e)
-        {           
-            BindingSource bs = new BindingSource();
-            bs.DataSource = dgvGrilla.DataSource;
-            bs.Filter ="Descripcion like '%" + (sender as TextBox).Text + "%'";
-            dgvGrilla.DataSource = bs;
-        }
+		private void dgvGrilla_CellClick(object sender, DataGridViewCellEventArgs e)
+		{
+			int intFila = e.RowIndex;
+			LlamadoAModificar(intFila);
+		}
 
-        #endregion
+		private void TextBox_Changed(object sender, EventArgs e)
+		{
+			BindingSource bs = new BindingSource();
+			bs.DataSource = dgvGrilla.DataSource;
+			bs.Filter = "Descripcion like '%" + (sender as TextBox).Text + "%'";
+			dgvGrilla.DataSource = bs;
+		}
 
-        #region "boton"
+		#endregion
 
-        #endregion
+		#region "boton"
 
-        #region Barra
+		#endregion
 
-        private void tsbGuardar_Click(object sender, EventArgs e)
-        {
-            Cursor = Cursors.WaitCursor;
+		#region Barra
 
-            Grabar();
+		private void tsbGuardar_Click(object sender, EventArgs e)
+		{
+			Cursor = Cursors.WaitCursor;
 
-            Cursor = Cursors.Default;
-        }
+			Grabar();
 
-        #endregion
+			Cursor = Cursors.Default;
+		}
 
-        private void dgvGrilla_ColumnWidthChanged(object sender, DataGridViewColumnEventArgs e)
-        {
-            txtBox.Width = Colperfil1.Width;
-        }
-    }
+		#endregion
+
+	}
 }
