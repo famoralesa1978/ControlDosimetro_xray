55674d68fb8b3cb743b06c1e2afc615843524396
felipe morales <fmorales_arenas@hotmail.com>
2020-12-16 22:43:38 -0400

    Configurar Correo

--- Mantenedor/frmBusquedaEmpresa.Designer.cs
+++ Mantenedor/frmBusquedaEmpresa.Designer.cs
@@ -46,6 +46,12 @@
 			this.label1 = new System.Windows.Forms.Label();
 			this.groupBox2 = new System.Windows.Forms.GroupBox();
 			this.grdDatos = new System.Windows.Forms.DataGridView();
+			this.N_Cliente = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Rut = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Razon_Social = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.colNombrefantasia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Direccion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Telefono = new System.Windows.Forms.DataGridViewTextBoxColumn();
 			this.cmsPopup = new System.Windows.Forms.ContextMenuStrip(this.components);
 			this.tsmEliminar = new System.Windows.Forms.ToolStripMenuItem();
 			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
@@ -54,12 +60,7 @@
 			this.tsbAgregarPersonal = new System.Windows.Forms.ToolStripButton();
 			this.tsbIngresoDosimetro = new System.Windows.Forms.ToolStripButton();
 			this.btnIngresarDosisISP = new System.Windows.Forms.ToolStripButton();
-			this.N_Cliente = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.Rut = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.Razon_Social = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.colNombrefantasia = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.Direccion = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.Telefono = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.tsbEmail = new System.Windows.Forms.ToolStripButton();
 			this.groupBox1.SuspendLayout();
 			this.groupBox2.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.grdDatos)).BeginInit();
@@ -82,50 +83,44 @@
 			this.groupBox1.Controls.Add(this.label3);
 			this.groupBox1.Controls.Add(this.txt_Rut);
 			this.groupBox1.Controls.Add(this.label1);
-			this.groupBox1.Location = new System.Drawing.Point(0, 34);
-			this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.groupBox1.Location = new System.Drawing.Point(0, 28);
 			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-			this.groupBox1.Size = new System.Drawing.Size(1132, 175);
+			this.groupBox1.Size = new System.Drawing.Size(849, 142);
 			this.groupBox1.TabIndex = 0;
 			this.groupBox1.TabStop = false;
 			this.groupBox1.Text = "Busqueda Cliente";
 			// 
 			// txt_NombreFantasia
 			// 
-			this.txt_NombreFantasia.Location = new System.Drawing.Point(131, 80);
-			this.txt_NombreFantasia.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.txt_NombreFantasia.Location = new System.Drawing.Point(98, 65);
 			this.txt_NombreFantasia.MaxLength = 60;
 			this.txt_NombreFantasia.Name = "txt_NombreFantasia";
-			this.txt_NombreFantasia.Size = new System.Drawing.Size(455, 22);
+			this.txt_NombreFantasia.Size = new System.Drawing.Size(342, 20);
 			this.txt_NombreFantasia.TabIndex = 38;
 			// 
 			// label5
 			// 
 			this.label5.AutoSize = true;
-			this.label5.Location = new System.Drawing.Point(11, 80);
-			this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label5.Location = new System.Drawing.Point(8, 65);
 			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(112, 17);
+			this.label5.Size = new System.Drawing.Size(84, 13);
 			this.label5.TabIndex = 39;
 			this.label5.Text = "Nombre fantasia";
 			// 
 			// txt_Direccion
 			// 
-			this.txt_Direccion.Location = new System.Drawing.Point(131, 108);
-			this.txt_Direccion.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.txt_Direccion.Location = new System.Drawing.Point(98, 88);
 			this.txt_Direccion.MaxLength = 60;
 			this.txt_Direccion.Name = "txt_Direccion";
-			this.txt_Direccion.Size = new System.Drawing.Size(455, 22);
+			this.txt_Direccion.Size = new System.Drawing.Size(342, 20);
 			this.txt_Direccion.TabIndex = 36;
 			// 
 			// label4
 			// 
 			this.label4.AutoSize = true;
-			this.label4.Location = new System.Drawing.Point(13, 111);
-			this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label4.Location = new System.Drawing.Point(10, 90);
 			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(67, 17);
+			this.label4.Size = new System.Drawing.Size(52, 13);
 			this.label4.TabIndex = 37;
 			this.label4.Text = "Dirección";
 			// 
@@ -133,10 +128,9 @@
 			// 
 			this.btn_cargarCliente.Image = global::ControlDosimetro.Properties.Resources.Buscar;
 			this.btn_cargarCliente.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-			this.btn_cargarCliente.Location = new System.Drawing.Point(613, 124);
-			this.btn_cargarCliente.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.btn_cargarCliente.Location = new System.Drawing.Point(460, 101);
 			this.btn_cargarCliente.Name = "btn_cargarCliente";
-			this.btn_cargarCliente.Size = new System.Drawing.Size(148, 49);
+			this.btn_cargarCliente.Size = new System.Drawing.Size(111, 40);
 			this.btn_cargarCliente.TabIndex = 35;
 			this.btn_cargarCliente.Text = "Busqueda";
 			this.btn_cargarCliente.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -145,19 +139,17 @@
 			// 
 			// txt_N_Cliente_Ref
 			// 
-			this.txt_N_Cliente_Ref.Location = new System.Drawing.Point(131, 21);
-			this.txt_N_Cliente_Ref.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.txt_N_Cliente_Ref.Location = new System.Drawing.Point(98, 17);
 			this.txt_N_Cliente_Ref.Name = "txt_N_Cliente_Ref";
-			this.txt_N_Cliente_Ref.Size = new System.Drawing.Size(132, 22);
+			this.txt_N_Cliente_Ref.Size = new System.Drawing.Size(100, 20);
 			this.txt_N_Cliente_Ref.TabIndex = 1;
 			// 
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(8, 25);
-			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Location = new System.Drawing.Point(6, 20);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(71, 17);
+			this.label2.Size = new System.Drawing.Size(54, 13);
 			this.label2.TabIndex = 34;
 			this.label2.Text = "N° Cliente";
 			// 
@@ -166,57 +158,51 @@
 			this.cbx_Estado.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
 			this.cbx_Estado.FlatStyle = System.Windows.Forms.FlatStyle.System;
 			this.cbx_Estado.FormattingEnabled = true;
-			this.cbx_Estado.Location = new System.Drawing.Point(131, 138);
-			this.cbx_Estado.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.cbx_Estado.Location = new System.Drawing.Point(98, 112);
 			this.cbx_Estado.Name = "cbx_Estado";
-			this.cbx_Estado.Size = new System.Drawing.Size(160, 24);
+			this.cbx_Estado.Size = new System.Drawing.Size(121, 21);
 			this.cbx_Estado.TabIndex = 4;
 			// 
 			// label11
 			// 
 			this.label11.AutoSize = true;
-			this.label11.Location = new System.Drawing.Point(16, 142);
-			this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label11.Location = new System.Drawing.Point(12, 115);
 			this.label11.Name = "label11";
-			this.label11.Size = new System.Drawing.Size(52, 17);
+			this.label11.Size = new System.Drawing.Size(40, 13);
 			this.label11.TabIndex = 31;
 			this.label11.Text = "Estado";
 			// 
 			// txt_RazonSocial
 			// 
-			this.txt_RazonSocial.Location = new System.Drawing.Point(131, 50);
-			this.txt_RazonSocial.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.txt_RazonSocial.Location = new System.Drawing.Point(98, 41);
 			this.txt_RazonSocial.MaxLength = 60;
 			this.txt_RazonSocial.Name = "txt_RazonSocial";
-			this.txt_RazonSocial.Size = new System.Drawing.Size(455, 22);
+			this.txt_RazonSocial.Size = new System.Drawing.Size(342, 20);
 			this.txt_RazonSocial.TabIndex = 3;
 			// 
 			// label3
 			// 
 			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(11, 50);
-			this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label3.Location = new System.Drawing.Point(8, 41);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(91, 17);
+			this.label3.Size = new System.Drawing.Size(70, 13);
 			this.label3.TabIndex = 4;
 			this.label3.Text = "Razon Social";
 			// 
 			// txt_Rut
 			// 
-			this.txt_Rut.Location = new System.Drawing.Point(405, 23);
-			this.txt_Rut.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.txt_Rut.Location = new System.Drawing.Point(304, 19);
 			this.txt_Rut.MaxLength = 10;
 			this.txt_Rut.Name = "txt_Rut";
-			this.txt_Rut.Size = new System.Drawing.Size(132, 22);
+			this.txt_Rut.Size = new System.Drawing.Size(100, 20);
 			this.txt_Rut.TabIndex = 2;
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(303, 27);
-			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Location = new System.Drawing.Point(227, 22);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(30, 17);
+			this.label1.Size = new System.Drawing.Size(24, 13);
 			this.label1.TabIndex = 0;
 			this.label1.Text = "Rut";
 			// 
@@ -226,11 +212,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.groupBox2.Controls.Add(this.grdDatos);
-			this.groupBox2.Location = new System.Drawing.Point(7, 217);
-			this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.groupBox2.Location = new System.Drawing.Point(5, 176);
 			this.groupBox2.Name = "groupBox2";
-			this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-			this.groupBox2.Size = new System.Drawing.Size(1284, 438);
+			this.groupBox2.Size = new System.Drawing.Size(963, 356);
 			this.groupBox2.TabIndex = 1;
 			this.groupBox2.TabStop = false;
 			this.groupBox2.Text = "Listado";
@@ -252,14 +236,70 @@
             this.Telefono});
 			this.grdDatos.ContextMenuStrip = this.cmsPopup;
 			this.grdDatos.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.grdDatos.Location = new System.Drawing.Point(4, 19);
-			this.grdDatos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.grdDatos.Location = new System.Drawing.Point(3, 16);
 			this.grdDatos.Name = "grdDatos";
 			this.grdDatos.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToFirstHeader;
 			this.grdDatos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-			this.grdDatos.Size = new System.Drawing.Size(1276, 415);
+			this.grdDatos.Size = new System.Drawing.Size(957, 337);
 			this.grdDatos.TabIndex = 7;
 			this.grdDatos.DoubleClick += new System.EventHandler(this.grdDatos_DoubleClick);
+			// 
+			// N_Cliente
+			// 
+			this.N_Cliente.DataPropertyName = "Id_Cliente";
+			this.N_Cliente.Frozen = true;
+			this.N_Cliente.HeaderText = "N°Cliente";
+			this.N_Cliente.Name = "N_Cliente";
+			this.N_Cliente.ReadOnly = true;
+			this.N_Cliente.Width = 76;
+			// 
+			// Rut
+			// 
+			this.Rut.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+			this.Rut.DataPropertyName = "Run";
+			this.Rut.Frozen = true;
+			this.Rut.HeaderText = "Rut";
+			this.Rut.Name = "Rut";
+			this.Rut.ReadOnly = true;
+			this.Rut.Width = 49;
+			// 
+			// Razon_Social
+			// 
+			this.Razon_Social.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+			this.Razon_Social.DataPropertyName = "Razon_Social";
+			this.Razon_Social.Frozen = true;
+			this.Razon_Social.HeaderText = "Razon Social";
+			this.Razon_Social.Name = "Razon_Social";
+			this.Razon_Social.ReadOnly = true;
+			this.Razon_Social.Width = 380;
+			// 
+			// colNombrefantasia
+			// 
+			this.colNombrefantasia.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+			this.colNombrefantasia.DataPropertyName = "Nombre_Fantasia";
+			this.colNombrefantasia.HeaderText = "Nombre fantasia";
+			this.colNombrefantasia.Name = "colNombrefantasia";
+			this.colNombrefantasia.ReadOnly = true;
+			this.colNombrefantasia.Width = 400;
+			// 
+			// Direccion
+			// 
+			this.Direccion.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+			this.Direccion.DataPropertyName = "Direccion";
+			this.Direccion.HeaderText = "Direccion";
+			this.Direccion.Name = "Direccion";
+			this.Direccion.ReadOnly = true;
+			this.Direccion.Width = 77;
+			// 
+			// Telefono
+			// 
+			this.Telefono.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+			this.Telefono.DataPropertyName = "Telefono";
+			this.Telefono.HeaderText = "Telefono";
+			this.Telefono.MinimumWidth = 80;
+			this.Telefono.Name = "Telefono";
+			this.Telefono.ReadOnly = true;
+			this.Telefono.Width = 80;
 			// 
 			// cmsPopup
 			// 
@@ -267,13 +307,13 @@
 			this.cmsPopup.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tsmEliminar});
 			this.cmsPopup.Name = "contextMenuStrip1";
-			this.cmsPopup.Size = new System.Drawing.Size(133, 28);
+			this.cmsPopup.Size = new System.Drawing.Size(118, 26);
 			// 
 			// tsmEliminar
 			// 
 			this.tsmEliminar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
 			this.tsmEliminar.Name = "tsmEliminar";
-			this.tsmEliminar.Size = new System.Drawing.Size(132, 24);
+			this.tsmEliminar.Size = new System.Drawing.Size(117, 22);
 			this.tsmEliminar.Text = "Eliminar";
 			this.tsmEliminar.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
 			this.tsmEliminar.Click += new System.EventHandler(this.tsmEliminar_Click);
@@ -283,13 +323,14 @@
 			this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
 			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tsbAgregarCliente,
+            this.tsbEmail,
             this.tsbAgregarReferencia,
             this.tsbAgregarPersonal,
             this.tsbIngresoDosimetro,
             this.btnIngresarDosisISP});
 			this.toolStrip1.Location = new System.Drawing.Point(0, 0);
 			this.toolStrip1.Name = "toolStrip1";
-			this.toolStrip1.Size = new System.Drawing.Size(1306, 27);
+			this.toolStrip1.Size = new System.Drawing.Size(980, 27);
 			this.toolStrip1.TabIndex = 2;
 			this.toolStrip1.Text = "toolStrip1";
 			// 
@@ -344,81 +385,33 @@
 			this.btnIngresarDosisISP.Image = ((System.Drawing.Image)(resources.GetObject("btnIngresarDosisISP.Image")));
 			this.btnIngresarDosisISP.ImageTransparentColor = System.Drawing.Color.Magenta;
 			this.btnIngresarDosisISP.Name = "btnIngresarDosisISP";
-			this.btnIngresarDosisISP.Size = new System.Drawing.Size(73, 24);
+			this.btnIngresarDosisISP.Size = new System.Drawing.Size(58, 24);
 			this.btnIngresarDosisISP.Text = "Dosis ISP";
 			this.btnIngresarDosisISP.TextImageRelation = System.Windows.Forms.TextImageRelation.Overlay;
 			this.btnIngresarDosisISP.Click += new System.EventHandler(this.btnIngresarDosisISP_Click);
 			// 
-			// N_Cliente
+			// tsbEmail
 			// 
-			this.N_Cliente.DataPropertyName = "Id_Cliente";
-			this.N_Cliente.Frozen = true;
-			this.N_Cliente.HeaderText = "N°Cliente";
-			this.N_Cliente.Name = "N_Cliente";
-			this.N_Cliente.ReadOnly = true;
-			this.N_Cliente.Width = 96;
-			// 
-			// Rut
-			// 
-			this.Rut.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
-			this.Rut.DataPropertyName = "Run";
-			this.Rut.Frozen = true;
-			this.Rut.HeaderText = "Rut";
-			this.Rut.Name = "Rut";
-			this.Rut.ReadOnly = true;
-			this.Rut.Width = 59;
-			// 
-			// Razon_Social
-			// 
-			this.Razon_Social.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
-			this.Razon_Social.DataPropertyName = "Razon_Social";
-			this.Razon_Social.Frozen = true;
-			this.Razon_Social.HeaderText = "Razon Social";
-			this.Razon_Social.Name = "Razon_Social";
-			this.Razon_Social.ReadOnly = true;
-			this.Razon_Social.Width = 380;
-			// 
-			// colNombrefantasia
-			// 
-			this.colNombrefantasia.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
-			this.colNombrefantasia.DataPropertyName = "Nombre_Fantasia";
-			this.colNombrefantasia.HeaderText = "Nombre fantasia";
-			this.colNombrefantasia.Name = "colNombrefantasia";
-			this.colNombrefantasia.ReadOnly = true;
-			this.colNombrefantasia.Width = 400;
-			// 
-			// Direccion
-			// 
-			this.Direccion.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
-			this.Direccion.DataPropertyName = "Direccion";
-			this.Direccion.HeaderText = "Direccion";
-			this.Direccion.Name = "Direccion";
-			this.Direccion.ReadOnly = true;
-			this.Direccion.Width = 96;
-			// 
-			// Telefono
-			// 
-			this.Telefono.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
-			this.Telefono.DataPropertyName = "Telefono";
-			this.Telefono.HeaderText = "Telefono";
-			this.Telefono.MinimumWidth = 80;
-			this.Telefono.Name = "Telefono";
-			this.Telefono.ReadOnly = true;
-			this.Telefono.Width = 93;
+			this.tsbEmail.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.tsbEmail.Image = global::ControlDosimetro.Properties.Resources.EmailConf;
+			this.tsbEmail.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.tsbEmail.Name = "tsbEmail";
+			this.tsbEmail.Size = new System.Drawing.Size(24, 24);
+			this.tsbEmail.Text = "Conf. correo";
+			this.tsbEmail.ToolTipText = "Configurar correo";
 			// 
 			// frmBusquedaEmpresa
 			// 
 			this.AcceptButton = this.btn_cargarCliente;
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(1306, 655);
+			this.ClientSize = new System.Drawing.Size(980, 532);
 			this.Controls.Add(this.toolStrip1);
 			this.Controls.Add(this.groupBox2);
 			this.Controls.Add(this.groupBox1);
-			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
-			this.MinimumSize = new System.Drawing.Size(1029, 560);
+			this.MinimumSize = new System.Drawing.Size(776, 462);
 			this.Name = "frmBusquedaEmpresa";
 			this.ShowInTaskbar = false;
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -467,5 +460,6 @@
 		private System.Windows.Forms.DataGridViewTextBoxColumn colNombrefantasia;
 		private System.Windows.Forms.DataGridViewTextBoxColumn Direccion;
 		private System.Windows.Forms.DataGridViewTextBoxColumn Telefono;
+		private System.Windows.Forms.ToolStripButton tsbEmail;
 	}
 }
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -183,6 +183,16 @@ namespace ControlDosimetro.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        public static System.Drawing.Bitmap EmailConf {
+            get {
+                object obj = ResourceManager.GetObject("EmailConf", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         public static System.Drawing.Bitmap excel {
             get {
                 object obj = ResourceManager.GetObject("excel", resourceCulture);
