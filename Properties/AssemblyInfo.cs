de84b8de5b0e9188992e7416219ffa8a8261ded6
felipe morales <fmorales@infomas.cl>
2023-07-26 23:43:51 -0400

    version 9.0.2.6

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o establecer como predeterminados los números de compilación y de revisión 
 // mediante el carácter '*', como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("9.0.2.5")]
+[assembly: AssemblyVersion("9.0.2.6")]
 [assembly: AssemblyFileVersion("9.0.0.5")]
