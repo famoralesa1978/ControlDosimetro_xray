a878f621b12bde415ab277cc2c4e71d597097e70
Felipe Morales <fmorales@infomas.cl>
2024-04-11 13:49:05 -0400

    Arreglo

--- Herramientas/frmLiberarDosimetro.Designer.cs
+++ Herramientas/frmLiberarDosimetro.Designer.cs
@@ -58,6 +58,7 @@
 			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
 			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
 			this.btn_EliminarSeleccion = new System.Windows.Forms.ToolStripMenuItem();
+			this.label3 = new System.Windows.Forms.Label();
 			this.grpDatosCliente.SuspendLayout();
 			this.cmsPopup.SuspendLayout();
 			this.contextMenuStrip1.SuspendLayout();
@@ -65,46 +66,42 @@
 			// 
 			// grpDatosCliente
 			// 
+			this.grpDatosCliente.Controls.Add(this.label3);
 			this.grpDatosCliente.Controls.Add(this.txt_NDos);
 			this.grpDatosCliente.Controls.Add(this.label1);
 			this.grpDatosCliente.Controls.Add(this.btn_filtro);
 			this.grpDatosCliente.Controls.Add(this.txt_NDoc);
 			this.grpDatosCliente.Controls.Add(this.btn_Guardar);
 			this.grpDatosCliente.Controls.Add(this.label2);
-			this.grpDatosCliente.Location = new System.Drawing.Point(7, 34);
-			this.grpDatosCliente.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.grpDatosCliente.Location = new System.Drawing.Point(5, 28);
 			this.grpDatosCliente.Name = "grpDatosCliente";
-			this.grpDatosCliente.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-			this.grpDatosCliente.Size = new System.Drawing.Size(576, 178);
+			this.grpDatosCliente.Size = new System.Drawing.Size(432, 186);
 			this.grpDatosCliente.TabIndex = 1;
 			this.grpDatosCliente.TabStop = false;
 			this.grpDatosCliente.Text = "Dato ";
 			// 
 			// txt_NDos
 			// 
-			this.txt_NDos.Location = new System.Drawing.Point(145, 53);
-			this.txt_NDos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.txt_NDos.Location = new System.Drawing.Point(107, 91);
 			this.txt_NDos.MaxLength = 10;
 			this.txt_NDos.Name = "txt_NDos";
-			this.txt_NDos.Size = new System.Drawing.Size(97, 22);
+			this.txt_NDos.Size = new System.Drawing.Size(74, 20);
 			this.txt_NDos.TabIndex = 2;
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(16, 57);
-			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Location = new System.Drawing.Point(10, 94);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(94, 17);
+			this.label1.Size = new System.Drawing.Size(70, 13);
 			this.label1.TabIndex = 47;
 			this.label1.Text = "N°  dosimetro";
 			// 
 			// btn_filtro
 			// 
-			this.btn_filtro.Location = new System.Drawing.Point(243, 113);
-			this.btn_filtro.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.btn_filtro.Location = new System.Drawing.Point(187, 137);
 			this.btn_filtro.Name = "btn_filtro";
-			this.btn_filtro.Size = new System.Drawing.Size(127, 28);
+			this.btn_filtro.Size = new System.Drawing.Size(95, 23);
 			this.btn_filtro.TabIndex = 4;
 			this.btn_filtro.Text = "Limpiar filtro";
 			this.btn_filtro.UseVisualStyleBackColor = true;
@@ -112,19 +109,17 @@
 			// 
 			// txt_NDoc
 			// 
-			this.txt_NDoc.Location = new System.Drawing.Point(145, 21);
-			this.txt_NDoc.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.txt_NDoc.Location = new System.Drawing.Point(107, 65);
 			this.txt_NDoc.MaxLength = 10;
 			this.txt_NDoc.Name = "txt_NDoc";
-			this.txt_NDoc.Size = new System.Drawing.Size(97, 22);
+			this.txt_NDoc.Size = new System.Drawing.Size(74, 20);
 			this.txt_NDoc.TabIndex = 1;
 			// 
 			// btn_Guardar
 			// 
-			this.btn_Guardar.Location = new System.Drawing.Point(83, 113);
-			this.btn_Guardar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.btn_Guardar.Location = new System.Drawing.Point(67, 137);
 			this.btn_Guardar.Name = "btn_Guardar";
-			this.btn_Guardar.Size = new System.Drawing.Size(152, 28);
+			this.btn_Guardar.Size = new System.Drawing.Size(114, 23);
 			this.btn_Guardar.TabIndex = 3;
 			this.btn_Guardar.Text = "Modificar estado";
 			this.btn_Guardar.UseVisualStyleBackColor = true;
@@ -133,10 +128,9 @@
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(16, 25);
-			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Location = new System.Drawing.Point(10, 68);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(102, 17);
+			this.label2.Size = new System.Drawing.Size(78, 13);
 			this.label2.TabIndex = 34;
 			this.label2.Text = "N°  documento";
 			// 
@@ -146,19 +140,19 @@
 			this.cmsPopup.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tsmEliminar});
 			this.cmsPopup.Name = "contextMenuStrip1";
-			this.cmsPopup.Size = new System.Drawing.Size(70, 26);
+			this.cmsPopup.Size = new System.Drawing.Size(68, 26);
 			// 
 			// tsmEliminar
 			// 
 			this.tsmEliminar.Name = "tsmEliminar";
-			this.tsmEliminar.Size = new System.Drawing.Size(69, 22);
+			this.tsmEliminar.Size = new System.Drawing.Size(67, 22);
 			// 
 			// toolStrip1
 			// 
 			this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
 			this.toolStrip1.Location = new System.Drawing.Point(0, 0);
 			this.toolStrip1.Name = "toolStrip1";
-			this.toolStrip1.Size = new System.Drawing.Size(595, 25);
+			this.toolStrip1.Size = new System.Drawing.Size(446, 25);
 			this.toolStrip1.TabIndex = 2;
 			this.toolStrip1.Text = "toolStrip1";
 			// 
@@ -280,21 +274,29 @@
 			this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.btn_EliminarSeleccion});
 			this.contextMenuStrip1.Name = "contextMenuStrip1";
-			this.contextMenuStrip1.Size = new System.Drawing.Size(70, 26);
+			this.contextMenuStrip1.Size = new System.Drawing.Size(68, 26);
 			// 
 			// btn_EliminarSeleccion
 			// 
 			this.btn_EliminarSeleccion.Name = "btn_EliminarSeleccion";
-			this.btn_EliminarSeleccion.Size = new System.Drawing.Size(69, 22);
+			this.btn_EliminarSeleccion.Size = new System.Drawing.Size(67, 22);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(13, 20);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(354, 13);
+			this.label3.TabIndex = 48;
+			this.label3.Text = "Se va eliminar el número de dosimetro  asociado al número de documento";
 			// 
 			// frmLiberarDosimetro
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(595, 228);
+			this.ClientSize = new System.Drawing.Size(446, 237);
 			this.Controls.Add(this.toolStrip1);
 			this.Controls.Add(this.grpDatosCliente);
-			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
 			this.Name = "frmLiberarDosimetro";
@@ -339,5 +341,6 @@
                                 private System.Windows.Forms.Button btn_filtro;
         private System.Windows.Forms.TextBox txt_NDos;
         private System.Windows.Forms.Label label1;
-    }
+		private System.Windows.Forms.Label label3;
+	}
 }
